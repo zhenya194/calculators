e9ed580eb3443cf4af9f52027c338f987e695333
zhenya194 <zhenya.pro1753@gmail.com>
2026-03-19 22:00:10 +0200

    Update python/terminal/main.py

--- python/terminal/main.py
+++ python/terminal/main.py
@@ -1,6 +1,6 @@
 while True:
     task:str = input("Write task: ")
-    if task.lower() == "exit":
+    if task.lower() == "exit" or task.lower() == "stop":
         break
     else:
         answer = eval(task)
