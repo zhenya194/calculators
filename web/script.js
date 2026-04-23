0fa7ebb3956500a887d34ba362a057bef8b8f153
zhenya194 <zhenya.pro1753@gmail.com>
2026-04-23 11:29:22 +0300

    Update requirements.txt

---
+++ web/script.js
@@ -0,0 +1,16 @@
+const label = document.getElementById("arithmetic")
+let text = "0"
+let res = 0
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+function writesymbol() {
+    for i in numbers {
+        if i == numb {
+            text += str
+        }
+    }
+    console.log("Wrote")
+}
+function result() {
+    res = eval(text)
+    label.textContent = res
+}
